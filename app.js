46d54dcdfc7c7527f75456868d8c392ba7f7e270
shvkumar7777 <43500125+shvkumar7777@users.noreply.github.com>
2021-10-01 23:15:39 +0530

    added the progressbar and the buttons functionality

--- app.js
+++ app.js
@@ -13,9 +13,23 @@ function updateDOM() {
             circle.classList.remove('active');
         }
     });
+
     //update the progress bar
+    let activecircles = document.querySelectorAll('.active');
+    console.log(activecircles.length / circles.length);
+    progressBar.style.width = (activecircles.length - 1) / (circles.length - 1) * 100 + '%';
 
     //update the button status
+    if (activecircles.length ==1) {
+        prevButton.disabled = true;
+    }
+    else if (activecircles.length == circles.length) {
+        nextButton.disabled = true;
+    } else {
+        prevButton.disabled = false;
+        nextButton.disabled = false;
+    }
+
 }
 
 //event listeners
@@ -33,5 +47,5 @@ prevButton.addEventListener('click', () => {
     if (currentActive == 1) {
         currentActive = 1;
     }
-
+    updateDOM();
 })
